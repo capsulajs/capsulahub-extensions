2bbeb1c7250035f9d063a3278734d408be68e136
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-04-19 11:28:55 +0300

    style fix

--- packages/logger/src/example/utils.ts
+++ packages/logger/src/example/utils.ts
@@ -1,18 +1,19 @@
 import { of, timer, merge } from "rxjs";
 import { delayWhen } from "rxjs/operators";
-import logs from '../../cypress/fixtures/logs.json';
+import logs from "../../cypress/fixtures/logs.json";
 
 export const props$ = of({
   logs$: merge(
-    ...logs.map(({ correlationId, type, serviceName, methodName, content, delay }, i) =>
-      of({
-        correlationId,
-        type,
-        serviceName,
-        methodName,
-        timestamp: new Date().getTime(),
-        data: { content }
-      }).pipe(delayWhen(() => timer(i * delay)))
+    ...logs.map(
+      ({ correlationId, type, serviceName, methodName, content, delay }, i) =>
+        of({
+          correlationId,
+          type,
+          serviceName,
+          methodName,
+          timestamp: new Date().getTime(),
+          data: { content }
+        }).pipe(delayWhen(() => timer(i * delay)))
     )
   )
 });
