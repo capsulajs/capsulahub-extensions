064f1aa155a17e6cffd7ff2eecb9000481d14e9c
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-04-19 11:28:35 +0300

    use fixtures

--- packages/logger/cypress/integration/logger.test.ts
+++ packages/logger/cypress/integration/logger.test.ts
@@ -1,28 +1,46 @@
 const { cy, describe, it } = global;
 
-const isValidateTimestamp = text =>
+const validateTimeFormat = text =>
   /^([0-9]{2})\:([0-9]{2}):([0-9]{2})$/.test(text);
+
+const checkRow = (row, log) => {
+  const { serviceName, methodName, content } = log;
+
+  row.find("[data-cy=logger-point-not-active]");
+  row.find("[data-cy=logger-arrows-green]");
+
+  expect(
+    validateTimeFormat(row.find("[data-cy=logger-timestamp]").text())
+  ).to.eq(true);
+  expect(row.find("[data-cy=logger-title]").text()).to.eq(
+    `${serviceName}/${methodName}`
+  );
+
+  expect(
+    row
+      .find("[data-cy=logger-content]")
+      .find(".string-value")
+      .text()
+  ).to.eq(`"${content}"`);
+};
 
 describe("Logger TCs", () => {
   it("Events are logged (check the format)", () => {
-    cy.visit("/")
-      .get("[data-cy=logger-row-0]")
-      .should(row => {
-        row.find("[data-cy=logger-point-not-active]");
-        row.find("[data-cy=logger-arrows-green]");
-
-        expect(
-          isValidateTimestamp(row.find("[data-cy=logger-timestamp]").text())
-        ).to.eq(true);
-        expect(row.find("[data-cy=logger-title]").text()).to.eq(
-          "AdeleService/hello$"
-        );
-        expect(
-          row
-            .find("[data-cy=logger-content]")
-            .find(".string-value")
-            .text()
-        ).to.eq('"Hello, it\'s me"');
-      });
+    cy.fixture("logs.json").then(logs => {
+      return cy
+        .visit("/")
+        .wait(logs[0].delay)
+        .get("[data-cy=logger-row-0]")
+        .should(row => checkRow(row, logs[0]))
+        .wait(logs[1].delay)
+        .get("[data-cy=logger-row-1]")
+        .should(row => checkRow(row, logs[1]))
+        .wait(logs[2].delay)
+        .get("[data-cy=logger-row-2]")
+        .should(row => checkRow(row, logs[2]))
+        .wait(logs[3].delay)
+        .get("[data-cy=logger-row-3]")
+        .should(row => checkRow(row, logs[3]));
+    });
   });
 });
--- packages/logger/src/example/utils.ts
+++ packages/logger/src/example/utils.ts
@@ -1,39 +1,19 @@
 import { of, timer, merge } from "rxjs";
 import { delayWhen } from "rxjs/operators";
+import logs from "../../cypress/fixtures/logs.json";
 
-const createAdeleEvent = (content, delay) =>
-  of({
-    correlationId: "Adele",
-    type: "request",
-    serviceName: "AdeleService",
-    methodName: "hello$",
-    timestamp: new Date().getTime(),
-    data: { content }
-  }).pipe(delayWhen(() => timer(delay)));
-
-const createSupportEvent = (content, delay) =>
-  of({
-    correlationId: "Support",
-    type: "response",
-    serviceName: "PhoneService",
-    methodName: "hello$",
-    timestamp: new Date().getTime(),
-    data: { content }
-  }).pipe(delayWhen(() => timer(delay)));
-
-export const content1 = "Hello, it's me";
-export const content2 =
-  "I was wondering if after all these years you'd like to meet";
-export const content3 =
-  "They say that time's supposed to heal ya, but I ain't done much healing";
-export const content4 = "Hello, can you hear me?";
-export const content5 = "No";
 export const props$ = of({
   logs$: merge(
-    createAdeleEvent(content1, 0),
-    createAdeleEvent(content2, 2500),
-    createAdeleEvent(content3, 5000),
-    createAdeleEvent(content4, 7500),
-    createSupportEvent(content5, 10000)
+    ...logs.map(
+      ({ correlationId, type, serviceName, methodName, content, delay }, i) =>
+        of({
+          correlationId,
+          type,
+          serviceName,
+          methodName,
+          timestamp: new Date().getTime(),
+          data: { content }
+        }).pipe(delayWhen(() => timer(i * delay)))
+    )
   )
 });
