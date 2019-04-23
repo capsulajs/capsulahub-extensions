9199e323a7e36d1156f10281b329580c40697178
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-04-23 16:35:46 +0300

    update capsula ui version

--- packages/canvas/src/example/utils.ts
+++ packages/canvas/src/example/utils.ts
@@ -4,6 +4,39 @@ import { delayWhen } from 'rxjs/operators';
 import { CanvasProps } from '../types';
 
 export const props$: Observable<CanvasProps> = of({
-  layout: {},
+  layout: {
+    id: 'root',
+    type: 'container',
+    flex: 0.5,
+    orientation: 'vertical',
+    nodes: [
+      {
+        id: 'node1',
+        type: 'element',
+        flex: 1,
+        tabs: [
+          {
+            id: 'tab1',
+            name: 'Tab 1',
+            content: '<web-cmponent-1></web-component-1>',
+          },
+        ],
+        activeTabIndex: 0,
+      },
+      {
+        id: 'node2',
+        type: 'element',
+        flex: 1,
+        tabs: [
+          {
+            id: 'tab2',
+            name: 'Tab 2',
+            content: '<web-cmponent-2></web-component-2>',
+          },
+        ],
+        activeTabIndex: 0,
+      },
+    ],
+  },
   onUpdate: console.log,
 });
