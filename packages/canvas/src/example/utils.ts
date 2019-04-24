b29316765b0cc5dffcb5bade5cc7a71be23900c9
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-04-24 11:56:26 +0300

    stylefix

--- packages/canvas/src/example/utils.ts
+++ packages/canvas/src/example/utils.ts
@@ -4,6 +4,6 @@ import layout from '../../cypress/fixtures/layout';
 import { Layout, CanvasProps } from '../types';
 
 const subject = new BehaviorSubject(layout);
-export const props$: Observable<CanvasProps> = subject.asObservable().pipe(
-  map((layout: Layout) => ({ layout, onUpdate: (layout: Layout) => subject.next(layout) }))
-);
+export const props$: Observable<CanvasProps> = subject
+  .asObservable()
+  .pipe(map((layout: Layout) => ({ layout, onUpdate: (layout: Layout) => subject.next(layout) })));
