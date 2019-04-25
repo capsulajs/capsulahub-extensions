0df979e5f3175c4254976f0716ee6511dfb83ec8
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-04-25 15:59:18 +0300

    add more test cases

--- packages/canvas/cypress/integration/canvas.test.js
+++ packages/canvas/cypress/integration/canvas.test.js
@@ -3,25 +3,58 @@ import { grapAllNodes } from './utils';
 const { cy, describe, it } = global;
 
 describe('Canvas TCs', () => {
-  it('Init', () => {
+  it('Each node can have one or more tabs according to tabs and activeTabIndex parameters', () => {
     cy.fixture('layout.js').then((layout) => {
-      return cy
-        .visit('/')
-        .get('[data-cy=canvas]')
-        .should((canvas) => {
-          grapAllNodes(layout).forEach(({ tabs, id: nodeId }) => {
-            tabs.forEach(({ name, id: tabId }) => {
-              const tab = canvas
-                .find(`[data-cy=canvas-node-${nodeId}]`)
-                .find('[data-cy=canvas-tabs]')
-                .find(`[data-cy=canvas-tab-${tabId}]`);
+      return cy.visit('/').then(() => {
+        grapAllNodes(layout).forEach(({ activeTabIndex, tabs, id: nodeId }) => {
+          tabs.forEach(({ name, id: tabId }, index) => {
+            const titleSelector =
+              index === activeTabIndex ? '[data-cy=canvas-tab-title-active]' : '[data-cy=canvas-tab-title]';
 
-              console.log(tab.text());
-
-              expect(tab.find('[data-cy=canvas-tab-title]').text()).to.eq(name);
-            });
+            cy.get(`[data-cy=canvas-node-${nodeId}]`)
+              .find('[data-cy=canvas-tabs]')
+              .find(`[data-cy=canvas-tab-${tabId}]`)
+              .find(titleSelector)
+              .invoke('text')
+              .should('eq', name);
           });
         });
+      });
+    });
+  });
+
+  it('The name of the tab can be edited manually by the user', () => {
+    cy.fixture('layout.js').then((layout) => {
+      const [{ tabs, id: nodeId }] = grapAllNodes(layout);
+      const [{ name, id: tabId }] = tabs;
+
+      return cy
+        .visit('/')
+        .get(`[data-cy=canvas-tab-${tabId}]`)
+        .find('[data-cy=canvas-tab-title-active]')
+        .dblclick()
+        .get(`[data-cy=canvas-tab-${tabId}]`)
+        .find('[data-cy=canvas-tab-input]')
+        .invoke('val')
+        .should('eq', name)
+        .get(`[data-cy=canvas-tab-${tabId}]`)
+        .find('[data-cy=canvas-tab-input]')
+        .clear()
+        .get(`[data-cy=canvas-tab-${tabId}]`)
+        .find('[data-cy=canvas-tab-input]')
+        .type('{enter}')
+        .get(`[data-cy=canvas-tab-${tabId}]`)
+        .find('[data-cy=canvas-tab-input]')
+        .invoke('val')
+        .should('eq', '')
+        .get(`[data-cy=canvas-tab-${tabId}]`)
+        .find('[data-cy=canvas-tab-input]')
+        .type('Test')
+        .type('{enter}')
+        .get(`[data-cy=canvas-tab-${tabId}]`)
+        .find('[data-cy=canvas-tab-title-active]')
+        .invoke('text')
+        .should('eq', 'Test');
     });
   });
 });
