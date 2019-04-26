4722e2f1e67f9f3c29eb6ce11f9bfe5406b73475
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-04-26 11:33:58 +0300

    fix linter

--- packages/canvas/src/canvas.tsx
+++ packages/canvas/src/canvas.tsx
@@ -6,8 +6,8 @@ import { dataComponentHoc } from '@capsulajs/web-components-utils';
 import { CanvasProps } from './types';
 
 export class Canvas extends HTMLElement {
-  private mountPoint: string;
   public props$;
+  private mountPoint: string;
 
   constructor() {
     super();
