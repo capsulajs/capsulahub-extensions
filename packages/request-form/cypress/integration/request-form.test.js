e0ddfebbf883ac493b7fcea836d4f86e4c5756d4
dmitriyslaym <dmitriyslaym@gmail.com>
2019-05-03 02:53:33 +0300

    Tests in progress

--- packages/request-form/cypress/integration/request-form.test.js
+++ packages/request-form/cypress/integration/request-form.test.js
@@ -1,3 +1,5 @@
+import { BehaviorSubject } from 'rxjs';
+
 const { cy, describe, it } = global;
 
 const defaultText = {
@@ -5,25 +7,68 @@ const defaultText = {
   json: '{}',
 };
 
-const editorsQuery = '#web-request-form .ace_content';
+const editors = '#web-request-form .ace_content';
 
 describe('Logger TCs', () => {
   it('Check the code pattern to be displayed for javascript language (with different number of arguments)', () => {
     cy.visit('/')
-      .get(editorsQuery)
-      .should('have.length', 1)
+      .checkEditorsAmount(1)
       .should('have.text', defaultText.javascript)
       .changeArgsAmount(2)
-      .get(editorsQuery)
-      .should('have.length', 3)
+      .checkEditorsAmount(2)
       .each((editor$) => expect(editor$.text()).to.equal(defaultText.javascript));
   });
 
-  // it('Check the code pattern to be displayed for json language (with different number of arguments)', () => {
-  //   cy.visit('/', {
-  //     onBeforeLoad(window) {},
-  //   });
-  //
-  //   cy.get('#web-request-form-wrapper .ace_content').should('have.text', defaultText.javascript);
-  // });
+  it('Check the code pattern to be displayed for json language (with different number of arguments)', () => {
+    cy.visit('/')
+      .changeLanguage('json')
+      .checkEditorsAmount(1)
+      .should('have.text', defaultText.json)
+      .changeArgsAmount(2)
+      .checkEditorsAmount(2)
+      .each((editor$) => expect(editor$.text()).to.equal(defaultText.json));
+  });
+
+  it('Check Request Form to be split according to the arguments number', () => {
+    cy.visit('/')
+      .checkEditorsAmount(1)
+      .changeArgsAmount(2)
+      .checkEditorsAmount(2)
+      .changeArgsAmount(3)
+      .checkEditorsAmount(3)
+      .changeLanguage('json')
+      .checkEditorsAmount(3)
+      .changeArgsAmount(1)
+      .checkEditorsAmount(1);
+  });
+
+  it.only('Submit with a valid request (check different data types in request and the model of data received)', () => {
+    const onSubmitSpy = cy.spy();
+    const basicProps = {
+      selectedMethodPath: '',
+      content: {
+        language: 'javascript',
+        requestArgs: 'return {};',
+      },
+      onSubmit: onSubmitSpy,
+    };
+    cy.visit('/', {
+      onBeforeLoad(window) {
+        window.requestFormPropsSubject = new BehaviorSubject(basicProps);
+      },
+    });
+
+    cy.typeInEditor('return 5;')
+      .submitRequest()
+      .wait(3500)
+      .then(() => {
+        expect(onSubmitSpy).calledWithExactly({ language: 'javascript', requestArgs: [5] });
+      })
+      .typeInEditor('return undefined;')
+      .submitRequest()
+      .wait(3500)
+      .then(() => {
+        expect(onSubmitSpy).calledWithExactly({ language: 'javascript', requestArgs: [undefined] });
+      });
+  });
 });
