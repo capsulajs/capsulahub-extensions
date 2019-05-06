4efbcc06d67bb39a0cdd470bf3114d87aca2b5d6
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-05-06 16:59:13 +0300

    style fix

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure, addDecorator } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 
-const req = require.context("../packages/", true, /stories\/.+.js$/);
+const req = require.context('../packages/', true, /stories\/.+.js$/);
 
 function loadStories() {
   addDecorator(withKnobs);
