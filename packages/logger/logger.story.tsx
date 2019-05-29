6fa8e86e19952ceb03a79c7ed216cada925e6528
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-05-29 11:43:58 +0300

    update tsconfig in storybook

--- packages/logger/logger.story.tsx
+++ packages/logger/logger.story.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { mountWebComponent } from './src/example/index';
+import { mountWebComponent } from './example/index';
 
 export const styles = {
   width: 1000,
