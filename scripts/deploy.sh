cd290ee1ee69e4c96ef68e1956d4411452782b3a
dmitriyslaym <dmitriyslaym@gmail.com>
2019-06-03 20:03:45 +0300

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,41 +1,22 @@
 #!/usr/bin/env bash
 S3_PATH="s3://$S3_BUCKET"
 SERVICE=$1
-SERVICE_PATH_TEST=""
 
-define_path() {
-    SLUG="/error/"
+# PR
+SLUG="/PR/$TRAVIS_PULL_REQUEST_BRANCH/"
+# master # develop
+if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+    SLUG="/$TRAVIS_BRANCH/"
+elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    SLUG="/rc/"
+fi;
 
-    # PR
-    if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
-        echo "define_path case 1"
-        SLUG="/PR/$TRAVIS_PULL_REQUEST_BRANCH/"
-    # master # develop
-    elif [[ "$TRAVIS_BRANCH" == "develop" ]]; then
-        echo "define_path case 2"
-        SLUG="/$TRAVIS_BRANCH/"
-    elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
-        echo "define_path case 3"
-        SLUG="/rc/"
-    fi;
-
-#    if [[ "$SLUG" == "/error/" ]]; then
-#        echo "define_path case 4"
-#        exit 1
-#    fi;
-
-    SERVICE_PATH_TEST="/extensions/$SERVICE$SLUG"
-}
-
-define_path
-
-SERVICE_PATH="/PR/enable-ci-cd/packages/$SERVICE/"
+SERVICE_PATH="$SLUG/extensions/$SERVICE"
 
 echo "current branch: $TRAVIS_BRANCH $TRAVIS_PULL_REQUEST_BRANCH is pull request: $TRAVIS_PULL_REQUEST"
 echo "S3 Path: $S3_PATH"
 echo "travis event type: $TRAVIS_EVENT_TYPE"
-echo "S3 URL: $S3_URL"
-echo "Cloudfront URL: $CF_URL"
+echo "S3 URL: $CF_BASE_URL$SERVICE_PATH"
 echo "SERVICE_PATH: $SERVICE_PATH"
 
 [[ -z $SERVICE_PATH ]] && echo "Error: Empty SERVICE_PATH" && exit 1
