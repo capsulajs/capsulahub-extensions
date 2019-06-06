177811480242d2031212adc4d6dc62eba939514d
dmitriyslaym <dmitriyslaym@gmail.com>
2019-06-06 08:51:05 +0300

    Fix webpack config

--- base-webpack.config-example.js
+++ base-webpack.config-example.js
@@ -1,13 +1,14 @@
-const path = require('path');
 const baseConfig = require('./base-webpack.config');
-const packagePath = path.resolve(__dirname, `packages/${process.env.PACKAGE_NAME}`);
 
-module.exports = {
-  ...baseConfig,
-  entry: `${packagePath}/public/index.ts`,
-  output: {
-    path: `${packagePath}/public/dist`,
-    filename: 'index.js',
-  },
-  plugins: [],
+module.exports = () => {
+  const packagePath = process.cwd();
+  return {
+    ...baseConfig,
+    entry: `${packagePath}/public/index.ts`,
+    output: {
+      path: `${packagePath}/public/dist`,
+      filename: 'index.js',
+    },
+    plugins: [],
+  };
 };
